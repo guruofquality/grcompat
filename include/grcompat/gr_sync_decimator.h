eab376be1d241a651ed41a2bedbe07213ad43b06
Tim O'Shea <tim.oshea753@gmail.com>
2013-05-31 18:59:51 -0400

    compat: adding gr_sync_interpolator/decimator

---
+++ include/grcompat/gr_sync_decimator.h
@@ -0,0 +1,14 @@
+//Its good to have some comments at the top of a file 
+
+#ifndef GNURADIO_GR_SYNC_DECIMATOR_H
+#define GNURADIO_GR_SYNC_DECIMATOR_H
+
+#include <gr_sync_block.h>
+#include <gnuradio/sync_decimator.h>
+
+typedef gr::sync_decimator gr_sync_decimator;
+
+#endif //GNURADIO_GR_SYNC_DECIMATOR_H
+
+#warning GR-COMPAT REQUIRED TO COMPILE - OUTDATED GNURADIO API IN USE - PLEASE UPDATE YOUR MODULE!!!
+
---
+++ include/grcompat/gr_sync_interpolator.h
@@ -0,0 +1,14 @@
+//Its good to have some comments at the top of a file 
+
+#ifndef GNURADIO_GR_SYNC_INTERPOLATOR_H
+#define GNURADIO_GR_SYNC_INTERPOLATOR_H
+
+#include <gr_sync_block.h>
+#include <gnuradio/sync_interpolator.h>
+
+typedef gr::sync_interpolator gr_sync_interpolator;
+
+#endif //GNURADIO_GR_SYNC_INTERPOLATOR_H
+
+#warning GR-COMPAT REQUIRED TO COMPILE - OUTDATED GNURADIO API IN USE - PLEASE UPDATE YOUR MODULE!!!
+
